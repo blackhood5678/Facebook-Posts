0d1ec4d9b4cb57ccc9192edb63ac15bf5f42cff2
nkrustev98 <n.krustev98@gmail.com>
2017-03-16 19:06:35 +0200

    postove
    Nov

--- Project/PHP/fb_posts.php
+++ Project/PHP/fb_posts.php
@@ -9,6 +9,19 @@
 		<meta charset="UTF-8">
 	</head>
 	<body>
+	
 		<a href="logout.php">Log Out</a>
+		
+		<form method="POST" action="">
+		<div>
+			<textarea name="text"></textarea>
+		</div>
+		<div>
+			<input type="file" name="file">
+		</div>
+		<div>
+			<input type="submit" nema="button" value="Send">
+		</div>
+		</form>
 	</body>
 </html>
